2b51d52981b28cf8c995db09c5aaab7c7b26c0b4
Alina <xla233@sfu.ca>
2019-01-29 16:20:07 -0800

    Alina - Buttons now have Spanish words on it.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,12 +2,40 @@ package com.example.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = "CMPT276-1191E1-Delta";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.fill_Span:
+                Log.d(TAG,"User choses to fill in Spanish");
+                return true;
+
+            case R.id.fill_Eng:
+                Log.d(TAG,"User choses to fill in English");
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
