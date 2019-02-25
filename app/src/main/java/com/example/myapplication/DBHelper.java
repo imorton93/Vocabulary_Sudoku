43819c08f2c18d281e946431be5e0b75f6ce9bf5
xla233 <xla233@sfu.ca>
2019-02-25 13:46:11 -0800

    Revert "Merge branch 'Aaron' into 'master'"
    This reverts merge request !22

--- app/src/main/java/com/example/myapplication/DBHelper.java
+++ app/src/main/java/com/example/myapplication/DBHelper.java
@@ -15,12 +15,10 @@ public class DBHelper extends SQLiteOpenHelper {
 
     private static final int VERSION = 1;
     private static final String DATABASE_NAME = "wordPairs.db";
-    private static final String TABLE_NAME_WORDS = "EngSpanWords";
-    private static final String TABLE_NAME_WRONG = "WrongWords";
+    private static final String TABLE_NAME = "EngSpanWords";
 
     private static final String ENG = "English";
     private static final String SPAN = "Spanish";
-    private static final String TOTAL = "wrongTotal";
 
     public DBHelper(Context context) {
         super(context, DATABASE_NAME, null, VERSION);
@@ -28,16 +26,14 @@ public class DBHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE " + TABLE_NAME_WORDS +  " ( English TEXT, Spanish TEXT)");
-        db.execSQL("CREATE TABLE " + TABLE_NAME_WRONG + " ( English TEXT, Spanish TEXT, wrongTotal INT)");
+        db.execSQL("CREATE TABLE " + TABLE_NAME +  " ( English TEXT, Spanish TEXT)");
         Log.d(TAG,"Created");
         Log.i("MYDB", "onCreate " + VERSION);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME_WORDS);
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME_WRONG);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
 
         Log.d(TAG,"deleted");
         Log.i("MYDB", "onUpgrade: old " + oldVersion + " new " + newVersion);
@@ -54,19 +50,8 @@ public class DBHelper extends SQLiteOpenHelper {
         ContentValues contentValues = new ContentValues();
         contentValues.put(ENG,Eng);
         contentValues.put(SPAN,Span);
-        db.insert(TABLE_NAME_WORDS,null,contentValues);
-        Log.d(TAG, "addData: Adding " + Eng+ Span + " to " + TABLE_NAME_WORDS);
-        return true;
-    }
-
-    public boolean updateWrongWord (String Eng, String Span, Integer total) {
-        SQLiteDatabase db = this.getWritableDatabase();
-        ContentValues contentValues = new ContentValues();
-        contentValues.put(ENG,Eng);
-        contentValues.put(SPAN,Span);
-        contentValues.put(TOTAL,total);
-        db.insert(TABLE_NAME_WRONG,null,contentValues);
-        Log.d(TAG, "addData: Adding " + Eng+ Span + " to " + TABLE_NAME_WRONG);
+        db.insert(TABLE_NAME,null,contentValues);
+        Log.d(TAG, "addData: Adding " + Eng+ Span + " to " + TABLE_NAME);
         return true;
     }
 
@@ -77,13 +62,13 @@ public class DBHelper extends SQLiteOpenHelper {
 
     public void onDestroy(){
         SQLiteDatabase db = this.getReadableDatabase();
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME_WORDS);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
         onCreate(db);
     }
 
     public ArrayList<String> getData(){
         SQLiteDatabase db = this.getReadableDatabase();
-        String query = "SELECT DISTINCT (English), (Spanish) FROM " + TABLE_NAME_WORDS;
+        String query = "SELECT DISTINCT (English), (Spanish) FROM " + TABLE_NAME;
         Cursor data = db.rawQuery(query, null);
 
         ArrayList<String> itemIds = new ArrayList<>();
@@ -92,25 +77,6 @@ public class DBHelper extends SQLiteOpenHelper {
             do {
                itemIds.add(data.getString(0));
                itemIds.add(data.getString(1));
-
-            } while (data.moveToNext());
-        }
-        data.close();
-        return itemIds;
-    }
-
-    public ArrayList<String> getDataWrong(){
-        SQLiteDatabase db = this.getReadableDatabase();
-        String query = "SELECT DISTINCT (English), (Spanish) FROM " + TABLE_NAME_WRONG;
-        Cursor data = db.rawQuery(query, null);
-
-        ArrayList<String> itemIds = new ArrayList<>();
-        if (data.moveToFirst()) {
-            //Loop through the table rows
-            do {
-                itemIds.add(data.getString(0));
-                itemIds.add(data.getString(1));
-                itemIds.add(data.getString(2));
 
             } while (data.moveToNext());
         }
