52a67b58a16de17d1e2d1ea0f4dd0c28660bc3de
imorton93 <46317377+imorton93@users.noreply.github.com>
2019-03-18 15:10:38 -0700

    6x6 grid

---
+++ app/src/main/java/com/example/myapplication/main12x12.java
@@ -0,0 +1,13 @@
+package com.example.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class main12x12 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main12x12);
+    }
+}
